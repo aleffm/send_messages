b6e0d906db70934d969ed814fdae110a27d4c68f
Aleff <aleff.matos@e-nexus.com.br>
2021-11-25 05:45:54 -0300

    Creation

---
+++ WebOpenApp/app/src/main/java/com/example/webopenapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.webopenapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.main_wv);
+        webView.loadUrl("https://eva.fmrp.usp.br/");
+
+
+    }
+}
